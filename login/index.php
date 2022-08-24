5367c0dc6dbcca022963adeccae0b4f2c0066c7f
KnDjoshua <78736301+KnDjoshua@users.noreply.github.com>
2022-08-24 09:03:20 +0300

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+<DOCTYPE html>
+    <html>
+        <head>
+            <title>MY WEBSITE</tittle>
+</head>
+<body>
+    <a href="logout.php">LOGOUT</a>
+    <h1>This is the index page</h1>
+</body>
+
+<br>
+Hello, Username.
+</html>
